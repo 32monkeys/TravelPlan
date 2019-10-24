1435d66f26eb94e066cc26bcac4a46b34ee7b8b0
32monkeys <38853524+32monkeys@users.noreply.github.com>
2019-10-24 12:23:14 +0200

    Textarea now gets text from DB

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,8 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 
+import java.sql.*;
+
 
 public class Controller {
 
@@ -23,20 +25,61 @@ public class Controller {
 
     public void initialize() {
         //executed when GUI is ready
-        CourseJDBC.startCourseJDBC();
+
+
         submitButt.setOnAction(event -> updateText());
-        for (int i=2;i<12;i++){
-            time.getItems().add(i+":00");
-            time.getItems().add(i+":30");
+        for (int i = 2; i < 12; i++) {
+            time.getItems().add(i + ":00");
+            time.getItems().add(i + ":30");
         }
 
-        from.getItems().addAll("København","Høje Tåstrup", "Roskilde", "Ringsted", "Odense", "Næstved", "Nykøbing F");
+        from.getItems().addAll("København", "Høje Tåstrup", "Roskilde", "Ringsted", "Odense", "Næstved", "Nykøbing F");
 
-        to.getItems().addAll("København","Høje Tåstrup", "Roskilde", "Ringsted", "Odense", "Næstved", "Nykøbing F");
+        to.getItems().addAll("København", "Høje Tåstrup", "Roskilde", "Ringsted", "Odense", "Næstved", "Nykøbing F");
     }
 
-    public void updateText(){
+    public void updateText() {
+        textArea.setText("");
+        runDatabase();
 
+    }
 
+    public void runDatabase() {
+        CourseJDBC CJ = new CourseJDBC();
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:Rejseplan.db";
+            conn = CJ.connect(url);
+            //Select;
+
+            PreparedStatement pstmt = CJ.selectPreparedStatement(conn);
+            ResultSet resultSet = pstmt.executeQuery();
+
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+            while (resultSet.next()) {
+                for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
+                    textArea.appendText(" " + resultSet.getString(i) + " ");
+                }
+                textArea.appendText("\n");
+                /*
+                String name = resultSet.getString("StationName");
+                textArea.appendText(name + "\n");
+                System.out.println(name);
+                */
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
+
--- src/sample/CourseJDBC.java
+++ src/sample/CourseJDBC.java
@@ -10,104 +10,13 @@ public class CourseJDBC {
         return DriverManager.getConnection(url);
     }
 
-    public void PresentResults(ResultSet res)
+  public PreparedStatement selectPreparedStatement(Connection conn)
             throws SQLException {
-        if (res == null)
-            System.out.println("No records for customer");
-        while (res != null && res.next()) {
-            String name = res.getString("StationName");
-            System.out.println(name);
-        }
+        String query = "select StationName, Arrival from Arrive "
+                + "where StationName = 'København' and Arrival > 800";
+        PreparedStatement selectPreparedStatement = null;
+        selectPreparedStatement = conn.prepareStatement(query);
+        return selectPreparedStatement;
     }
 
-    public ResultSet plainstatement(String student, Connection conn)
-            throws SQLException {
-        String query = "select Students.Name As Name, Courses.Name As Course, Study.Grade As Grade from Study" +
-                " INNER JOIN Students ON Students.Studentid = Study.Studentid " +
-                "INNER JOIN Courses ON Courses.Courseid = Study.Courseid" +
-                " Where Students.Name = '" + student + "'";
-        Statement stmt = null;
-        ResultSet res = null;
-        stmt = conn.createStatement();
-        res = stmt.executeQuery(query);
-        return res;
-    }
-
-    public PreparedStatement selectpreparedstatement(Connection conn)
-            throws SQLException {
-        String query = "select StationName from Stations";
-        PreparedStatement selectpstmt = null;
-        selectpstmt = conn.prepareStatement(query);
-        return selectpstmt;
-    }
-
-    public PreparedStatement Updatepreparedstatement(Connection conn)
-            throws SQLException {
-        String sqlUpdate = "UPDATE Study "
-                + " SET Grade = ? "
-                + " WHERE (Studentid, Courseid) "
-                + " IN ( SELECT s.Studentid,c.Courseid "
-                + " FROM Students As s, Courses As c"
-                + " WHERE s.Name= ? AND c.Name = ? )";
-        PreparedStatement updatepstmt = null;
-        updatepstmt = conn.prepareStatement(sqlUpdate);
-        return updatepstmt;
-    }
-
-    public PreparedStatement InsertStudentpreparedstatement(Connection conn)
-            throws SQLException {
-        String sqlInsert = "INSERT INTO Students (name, town , age, sex) VALUES(?,?,?,?)";
-        PreparedStatement insertpstmt = null;
-        insertpstmt = conn.prepareStatement(sqlInsert);
-        return insertpstmt;
-    }
-
-    public PreparedStatement InsertCoursepreparedstatement(Connection conn)
-            throws SQLException {
-        String sqlInsert = "INSERT INTO Courses (Courseid,Name, Teacher, Description, Semester) VALUES(?,?,?,?,?)";
-        PreparedStatement insertpstmt = null;
-        insertpstmt = conn.prepareStatement(sqlInsert);
-        return insertpstmt;
-    }
-
-    public PreparedStatement InsertStudypreparedstatement(Connection conn)
-            throws SQLException {
-        String sqlInsert = "INSERT INTO Study (Studentid, Courseid, Grade) SELECT s.Studentid,c.Courseid, ? " +
-                "    FROM Students As s, Courses As c " +
-                "        WHERE s.Name= ? AND c.Name = ? ;";
-        PreparedStatement insertpstmt = null;
-        insertpstmt = conn.prepareStatement(sqlInsert);
-        return insertpstmt;
-    }
-
-
-    public static void startCourseJDBC() {
-        CourseJDBC CJ = new CourseJDBC();
-        Connection conn = null;
-        try {
-            String url = "jdbc:sqlite:Rejseplan.db";
-            conn = CJ.connect(url);
-// con.setAutoCommit (false);
-            //Select;
-            // PreparedStatement pstmt = CJ.Updatepreparedstatement(conn);
-
-            PreparedStatement pstmt = CJ.selectpreparedstatement(conn);
-            //int rowAffected = pstmt.executeUpdate();
-            //System.out.println(String.format("Rows affected %d", rowAffected));
-            ResultSet res = pstmt.executeQuery();
-            System.out.println("Query done");
-            CJ.PresentResults(res);
-            pstmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
 }
