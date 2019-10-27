dcd011ee9e015f754530abd1682b531c5835cdc3
32monkeys <38853524+32monkeys@users.noreply.github.com>
2019-10-27 15:06:54 +0100

    comment

--- src/files/Database.java
+++ src/files/Database.java
@@ -13,7 +13,7 @@ public class Database {
   public PreparedStatement selectPreparedStatement(Connection conn)
             throws SQLException {
         // select query
-        // join A.TrainID on B.TrainID limit us to direct routes without transfers
+        // join A.TrainID = B.TrainID limit us to direct routes without transfers
         String query = "select A.StationName as start, B.StationName as dest, A.Arrival as departure, B.Arrival as arrival "
                 + "from Arrive as A "
                 + "join (select TrainID, StationName, Arrival from Arrive "
